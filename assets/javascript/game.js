8766ddb841d04e79069bfbc0013a86a1bff245c9
Tez, Gardner <tezgardner1212@gmail.com>
2019-05-17 09:55:18 -0500

    Html and Random Number function complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    var wins = 0
+    var losses=0
+
+
+    
+    function getRandNum(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+      }
+
+      console.log(getRandNum(9,120))
+
+      
+
+
+
+
+
+});
