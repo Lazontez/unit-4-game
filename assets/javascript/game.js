ea1402d836641d787661d87c038cd444a8a8c845
Tez, Gardner <tezgardner1212@gmail.com>
2019-05-18 13:31:59 -0500

    Added Readme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,26 +5,37 @@ $(document).ready(function () {
     // var randomResult;
     var currentNum = 0;
 
-    var initializeGame = function () {
-        $(".crystals").empty()
-        randomResult = Math.floor(Math.random() * 90) + 30
-        //makes random Result a function that finds a random number
-        //  randomResult = function getRandNumToGuess(min, max) {
-        //     return Math.floor(Math.random() * (max - min)) + min;
+    var crystalImages = [
+        "assets/images/crystalCollector1.jpg",
+        "assets/images/crystalCollector2.jpg",
+        "assets/images/crystalCollector3.jpg",
+        "assets/images/crystalCollector4.jpg"
+    ]
 
-        // }
+    var initializeGame = function () {
+
+        //emptys the crystal attribute to restart game
+
+        $(".crystals").empty()
+
+        //This  sets the value of randomResult to a random number from 19-120
+        randomResult = Math.floor(Math.random() * 120) + 19
+        console.log("random number ="+ randomResult)
+        
+
+        
         //grabs the numberdiplay ID from the Html and calls on the 
         //randomResult as the content of the ID
         $("#numberDisplay").html(randomResult)
         // (19,120)
 
         //Runs a loop 4 times in order to create divs for the crystals
-        //it then makes a random number variable from 1-12 and stores that in 
-        //crystal <div>
+        //it then makes a random number variable from 1-12 and stores that in the
+        //crystal <div> I created
         for (i = 0; i < 4; i++) {
-            
+
             var crystalRandomNum = Math.floor(Math.random() * 11) + 1;
-            
+
 
             var crystal = $("<div>")
             crystal.attr({
@@ -32,21 +43,28 @@ $(document).ready(function () {
                 "randomNumData": crystalRandomNum
 
             })
+            
+
+            crystal.css({
+                "background-image": "src('"+ crystalImages[i] +"')",
+
+            })
+
             $(".crystals").append(crystal)
         }
         $("#totalScore").html(currentNum)
     }
 
-        initializeGame()
+    initializeGame()
 
 
-        
+
 
 
     //ON CLICK it makes a variable called crystal number and grabs the randomNumData
     //which holds the crystalRandomNum variable and turns that into a number
     //it then adds the currentNum variable to the crystal number variable I created
-    $(document).on("click",".crystal" , function () {
+    $(document).on("click", ".crystal", function () {
 
 
         var crystalNum = parseInt($(this).attr("randomNumData"));
@@ -58,24 +76,24 @@ $(document).ready(function () {
         if (currentNum > randomResult) {
             loss++
             $("#losses").html(+loss)
-            currentNum=0
-            console.log("Losses= "+ loss)
+            currentNum = 0
+            console.log("Losses= " + loss)
             alert("You Lose")
             initializeGame()
-            
+
         }
         else if (currentNum === randomResult) {
             wins++
-            
-            $("#wins").html( wins)
-            console.log("Wins= "+ wins)
-            currentNum=0
+
+            $("#wins").html(wins)
+            console.log("Wins= " + wins)
+            currentNum = 0
             alert("You Win")
             initializeGame()
         }
-        
 
-        
+
+
     })
 
 
